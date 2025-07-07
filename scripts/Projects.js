de703e9486c18e56d927c89de22cfa74140afd5a
Tobechi Duru <tobechi.duru001@gmail.com>
2025-07-07 15:05:33 -0700

    Add project and top project scripts, update HTML to include new scripts and enhance carousel functionality

---
+++ scripts/Projects.js
@@ -0,0 +1,158 @@
+// Carousel for projects page
+
+const Projects = [{
+  url: "https://jumia-clone-sl3f.vercel.app/",
+  thumbnails: "58EC0FDB-8F9F-486E-A555-FFAD982FA41D",
+  tags: {
+    tag1: "REACT UI",
+    tag2: "STRIPE PAYMENTS",
+    tag3: "NODE/EXPRESS"
+  },
+  description: "A modern online store with product pages, cart, and checkout",
+  title: "SHOPEASE (E-COMMERCE STORE)"
+}, 
+{
+  url: "https://jumia-clone-sl3f.vercel.app/",
+  thumbnails: "Tasknet",
+  tags: {
+    tag1: "REACT",
+    tag2: "NODE/EXPRESS API",
+    tag3: "MONGODB"
+  },
+  description: "Create, edit, and organize tasks with drag-and-drop UI.",
+  title: "TASKNEST (TASK MANAGER)"
+}, 
+{
+  url: "https://jumia-clone-sl3f.vercel.app/",
+  thumbnails: "skillHive",
+  tags: {
+    tag1: "REACT UI",
+    tag2: "NODE/EXPRESS",
+    tag3: "MONGODB"
+  },
+  description: "List and browse beginner-friendly online courses",
+  title: "SKILLHIVE (COURSE LISTING APP)"
+}, 
+{
+  url: "",
+  thumbnails: "eventitfy", 
+  tags: {
+    tag1: "NODE/EXPRESS",
+    tag2: "REACT",
+    tag3: "MONGODB"
+  },
+  description: "Create, rsvp to, or view events in your area.",
+  title: "EVENTIFY (EVENT MANAGEMENT APP)",
+}, 
+{
+  url: "",
+  thumbnails: "Notebox",
+  tags: {
+    tag1: "JWT-AUTH",
+    tag2: "EXPRESS API",
+    tag3: "MONGODB"
+  },
+  description: "Users can write, edit, and save personal notes in the cloud",
+  title: "NOTEBOX (NOTE-TAKING APP)"
+}, 
+{
+  url: "",
+  thumbnails: "Devlink",
+  tags: {
+    tag1: "REACT FORMS",
+    tag2: "EXPRESS API",
+    tag3: "MONGODB"
+  },
+  description: "A platform to create and view mini-developer profiles with social links",
+  title: "DEVLINK (DEVELOPER PROFILE DIRECTORY)"
+},
+{
+  url: "",
+  thumbnails: "recipe-vault",
+  tags: {
+    tag1: "REACT",
+    tag2: "MONGODB",
+    tag3: "EXPRESS"
+  },
+  description: "Search recipes via a 3rd party api, and save personal ones",
+  title: "RECIPEVAULT (RECIPE FINDER & MANAGER)"
+}, {
+  url: "",
+  thumbnails: "Movie",
+  tags: {
+    tag1: "CLOUDINARY",
+    tag2: "MONGODB",
+    tag3: "EXPRESS API"
+  },
+  description: "Search and view a collection of movies",
+  title: "MOVIEMATE"
+}, 
+{
+  url: "",
+  thumbnails: "chatify",
+  tags: {
+    tag1: "REACT CHAT UI",
+    tag2: "EXPRESS",
+    tag3: "MONGODB"
+  },
+  description: "One-on-one or group chat using socket.io",
+  title: "CHATIFY (REAL-TIME CHAT APP)"
+}, 
+{
+  url: "",
+  thumbnails: "pollster",
+  tags: {
+    tag1: "REACT",
+    tag2: "EXPRESS API",
+    tag3: "MONGODB"
+  },
+  description: "Create and vote on polls in real-time with live updates",
+  title: "POLLSTER (LIVE POLLING APP)"
+}, 
+{
+  url: "",
+  thumbnails: "blogify",
+  tags: {
+    tag1: "REACT",
+    tag2: "EXPRESS API",
+    tag3: "MONGODB"
+  },
+  description: "Create, read, update, delete blog posts with markdown support",
+  title: "BLOGIFY (SIMPLE BLOGGING PLATFORM)"
+}, 
+{
+  url: "",
+  thumbnails: "Admin",
+  tags: {
+    tag1: "REACT FRONTEND",
+    tag2:"EXPRESS API",
+    tag3: "MONGODB"
+  },
+  description: "An intuitive FINTECH Admin Dashboard",
+  title: "NEXARIC"
+}];
+
+
+
+
+let ProjectsHTML = '';
+
+Projects.forEach((Projects) =>{
+  ProjectsHTML += `<div class="card1 fade-in-up">
+                    <a href="${Projects.url}" style="text-decoration: none;">
+                        <div class="card-image1" style="background-image: url(Thumbnails/${Projects.thumbnails}.PNG)"></div>
+                        <div class="card-content1">
+                            <div class="card-tags1">
+                                <span class="tag1">${Projects.tags.tag1}</span>
+                                <span class="tag1">${Projects.tags.tag2}</span>
+                                <span class="tag1">${Projects.tags.tag3}</span>
+                            </div>
+                            <h3 class="card-title1">${Projects.description}</h3>
+                            <p class="card-date1">${Projects.title}</p>
+                        </div>
+                    </a>
+                </div>`
+});
+
+document.querySelector('.js-carousel').innerHTML = ProjectsHTML;
+
---
+++ scripts/topProjects.js
@@ -0,0 +1,157 @@
+// Carousel for Projects section on landing page
+
+
+const topProjects = [{
+  url: "https://jumia-clone-sl3f.vercel.app/",
+  thumbnails: "58EC0FDB-8F9F-486E-A555-FFAD982FA41D",
+  tags: {
+    tag1: "REACT UI",
+    tag2: "STRIPE PAYMENTS",
+    tag3: "NODE/EXPRESS"
+  },
+  description: "A modern online store with product pages, cart, and checkout",
+  title: "SHOPEASE (E-COMMERCE STORE)"
+}, 
+{
+  url: "",
+  thumbnails: "recipe-vault",
+  tags: {
+    tag1: "REACT",
+    tag2: "MONGODB",
+    tag3: "EXPRESS"
+  },
+  description: "Search recipes via a 3rd party api, and save personal ones",
+  title: "RECIPEVAULT (RECIPE FINDER & MANAGER)"
+},
+{
+  url: "",
+  thumbnails: "Devlink",
+  tags: {
+    tag1: "REACT FORMS",
+    tag2: "EXPRESS API",
+    tag3: "MONGODB"
+  },
+  description: "A platform to create and view mini-developer profiles with social links",
+  title: "DEVLINK (DEVELOPER PROFILE DIRECTORY)"
+}, 
+{
+  url: "",
+  thumbnails: "eventitfy",
+  tags: {
+    tag1: "NODE/EXPRESS",
+    tag2: "REACT",
+    tag3: "MONGODB"
+  },
+  description: "Create, rsvp to, or view events in your area.",
+  title: "EVENTIFY (EVENT MANAGEMENT APP)"
+},
+{
+  url: "",
+  thumbnails: "Notebox",
+  tags: {
+    tag1: "JWT-AUTH",
+    tag2: "EXPRESS API",
+    tag3: "MONGODB"
+  },
+  description: "Users can write, edit, and save personal notes in the cloud",
+  title: "NOTEBOX (NOTE-TAKING APP)"
+}, 
+{
+  url: "",
+  thumbnails: "skillHive",
+  tags: {
+    tag1: "REACT UI",
+    tag2: "NODE/EXPRESS",
+    tag3: "MONGODB"
+  },
+  description: "List and browse beginner-friendly online courses",
+  title: "SKILLHIVE (COURSE LISTING APP)"
+},
+{
+  url: "",
+  thumbnails: "Movie",
+  tags: {
+    tag1: "CLOUDINARY",
+    tag2: "MONGODB",
+    tag3: "EXPRESS API"
+  },
+  description: "Search and view a collection of movies",
+  title: "MOVIEMATE"
+}, 
+{
+  url: "",
+  thumbnails: "58EC0FDB-8F9F-486E-A555-FFAD982FA41D",
+  tags: {
+    tag1: "REACT UI",
+    tag2: "STRIPE PAYMENTS",
+    tag3: "NODE/EXPRESS"
+  },
+  description: "A modern online store with product pages, cart, and checkout",
+  title: "SHOPEASE (E-COMMERCE STORE)"
+},
+{
+  url: "",
+  thumbnails: "recipe-vault",
+  tags: {
+    tag1: "REACT",
+    tag2: "MONGODB",
+    tag3: "EXPRESS"
+  },
+  description: "Search recipes via a 3rd party api, and save personal ones",
+  title: "RECIPEVAULT (RECIPE FINDER & MANAGER)"
+}, 
+{
+  url: "",
+  thumbnails: "Devlink",
+  tags: {
+    tag1: "REACT FORMS",
+    tag2: "EXPRESS API",
+    tag3: "MONGODB"
+  },
+  description: "A platform to create and view mini-developer profiles with social links",
+  title: "DEVLINK (DEVELOPER PROFILE DIRECTORY)"
+},
+{
+  url: "",
+  thumbnails: "eventitfy",
+  tags: {
+    tag1: "NODE/EXPRESS",
+    tag2: "REACT",
+    tag3: "MONGODB"
+  },
+  description: "Create, rsvp to, or view events in your area.",
+  title: "EVENTIFY (EVENT MANAGEMENT APP)"
+}];
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    let topProjectsHTML = '';
+
+    topProjects.forEach((project) => {
+
+      topProjectsHTML += `<div class="card">
+                            <a href="${project.url}" style="text-decoration: none;">
+                                <div class="card-image" style="background-image: url(Thumbnails/${project.thumbnails}.PNG)"></div>
+                                <div class="card-content">
+                                    <div class="card-tags">
+                                        <span class="tag">${project.tags.tag1}</span>
+                                        <span class="tag">${project.tags.tag2}</span>
+                                        <span class="tag">${project.tags.tag3}</span>
+                                    </div>
+                                    <h3 class="card-title">${project.description}</h3>
+                                    <p class="card-date">${project.title}</p>
+                                </div>
+                            </a>
+                        </div>`
+    });
+
+    document.querySelector('.js-topProjects-carousel').innerHTML = topProjectsHTML;
+   
+
+    console.log(topProjectsHTML);
+})
+
+
+
