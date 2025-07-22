2068f39707d2f68c6f0fee614eceeb33007d68fd
Tobechi Duru <tobechi.duru001@gmail.com>
2025-07-21 18:00:10 -0700

    Completed blog section by linking all blogs to personal blog space on Hashnode

--- Scripts/blogs.js
+++ Scripts/blogs.js
@@ -2,27 +2,32 @@
 let blogSlider = [{
     thumbnails: "mern.jpg",
     header: "Building from the Ground Up — My Dev Philosophy",
-    description: "Why I Choose the MERN Stack, and What It Taught Me"
+    description: "Why I Choose the MERN Stack, and What It Taught Me",
+    id: 1,
   },
   {
     thumbnails: "prompt2.jpg",
     header: "Talking to Machines: My Prompt Engineering Deep Dive",
-    description: "How I Learned to Speak AI"
+    description: "What I’ve Learned About Getting the Best Out of AI Tools",
+    id: 2,
   },
   {
     thumbnails: "web333.jpg",
     header: "My Web3 Wake-Up Call",
-    description: "Stepping into Web3: My First Glimpse into the Decentralized Web"
+    description: "What I Got Wrong, What I Got Right, and Why It Matters Now",
+    id: 3,
   },
   {
     thumbnails: "deploy.png",
     header: "The Day My Code Went Live",
     description: "Deploying My First App: The Mistakes, the Fixes, the Lessons",
+    id: 4,
   },
   {
     thumbnails: "github.jpg",
     header: "GitHub Saved My Career (Literally)",
-    description: "How Version Control Helped Me Land Clients, Collaborate Better, and Never Lose a File Again"
+    description: "How Version Control Helped Me Land Clients, Collaborate Better, and Never Lose a File Again",
+    id: 5,
   }];
 
 
@@ -34,10 +39,41 @@ let blogSlider = [{
       `<div class="blog-slide-card" style="background-image: url('Thumbnails/${cards.thumbnails}');">
             <h2 style="color: gray">${cards.header}</h2>
             <p>${cards.description}</p>
-            <button onclick="alert('Blog contents is being updated! Check back later.')">Read Article</button>
+            <a><button class="js-readArticle" data-blogs-id = "${cards.id}">Read Article</button></a>
           </div>`
     })
 
     document.querySelector('.js-blogsWrapper').innerHTML = blogSliderHTML;
 
+    // View All Blogs
 
+    document.addEventListener('DOMContentLoaded', () => {
+      const blogBtn = document.querySelector('.js-readall');
+      if (blogBtn){
+        blogBtn.addEventListener('click', () => {
+          window.location.href="https://tobechiduru.hashnode.dev/";
+        })
+      }
+    })
+
+    // View Each Blog
+
+    document.addEventListener('DOMContentLoaded', () => {
+      document.querySelectorAll('.js-readArticle')
+        .forEach((button) => {
+          button.addEventListener('click', ()=> {
+            let blogsId = parseInt(button.dataset.blogsId);
+            if(blogsId === 1){
+            window.location.href="https://tobechiduru.hashnode.dev/building-from-the-ground-up-my-dev-philosophy";
+            } else if (blogsId === 2){
+                window.location.href="https://tobechiduru.hashnode.dev/talking-to-machines-my-prompt-engineering-deep-dive";
+            } else if(blogsId === 3){
+                window.location.href="https://tobechiduru.hashnode.dev/my-web3-wake-up-call";
+            } else if(blogsId === 4){
+                window.location.href="https://tobechiduru.hashnode.dev/the-day-my-code-went-live";
+            } else if(blogsId === 5){
+                window.location.href="https://tobechiduru.hashnode.dev/github-saved-my-career-literally";
+            };
+          });
+        });
+    });
