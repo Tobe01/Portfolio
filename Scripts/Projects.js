71e80ceeeff7eed319b111cf4a4dd3e45f0e0d0f
Tobechi Duru <tobechi.duru001@gmail.com>
2025-08-12 14:50:12 -0700

    merged

--- Scripts/Projects.js
+++ Scripts/Projects.js
@@ -91,8 +91,4 @@ const projectsContainer = {
     }  
 }
 
-projectsContainer.renderProjects();  
-
-document.addEventListener('DOMContentLoaded', () => {
-  projectsContainer.renderProjects();
-});
+projectsContainer.renderProjects();
